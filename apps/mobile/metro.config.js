83f6e18564cafbab723511a77e879a014be83f47
Dinner Swipe VPS <DSAsupport@dcss.dev>
2026-08-07 20:36:23 +0000

    Fix Expo font resolution for dev client

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const { getDefaultConfig } = require("expo/metro-config");
 
 const projectRoot = __dirname;
@@ -11,5 +12,21 @@ config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(workspaceRoot, "node_modules")
 ];
+config.resolver.disableHierarchicalLookup = true;
+config.resolver.extraNodeModules = new Proxy(
+  {},
+  {
+    get: (_target, moduleName) => {
+      if (typeof moduleName !== "string") {
+        return undefined;
+      }
+
+      const appModulePath = path.resolve(projectRoot, "node_modules", moduleName);
+      return fs.existsSync(appModulePath)
+        ? appModulePath
+        : path.resolve(workspaceRoot, "node_modules", moduleName);
+    }
+  }
+);
 
 module.exports = config;
